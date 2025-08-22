9c81e563afaf67d0fb72bedf2ca786cd6dd71491
Jake-CL <fariash@hotmail.com>
2025-08-21 21:55:23 -0400

    Se actualizaron los archivos css y js

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react';
+import SearchBar from './components/SearchBar/SearchBar';
+import SongList from './components/SongList/SongList';
+import Player from './components/Player/Player';
+import { searchTracks, searchAlbums, searchArtists } from './api/musicApi';
+import styles from './styles/layout.module.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+const [searchResults, setSearchResults] = useState([]);
+const [currentSongUrl, setCurrentSongUrl] = useState(null);
+// https://cors-anywhere.herokuapp.com/corsdemo 
+const handleSearch = async (searchTerm, filterType) => {
+setSearchResults([]);
+if (searchTerm) {
+let results = [];
+switch (filterType) {
+case 'album':
+results = await searchAlbums(searchTerm);
+break;
+case 'artist':
+results = await searchArtists(searchTerm);
+break;
+default:
+results = await searchTracks(searchTerm);
+break;
+}
+setSearchResults(results);
+}
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const handleSongSelect = (previewUrl) => {
+setCurrentSongUrl(previewUrl);
+};
+
+return (
+   
+<div className={styles.container}>
+ 
+<SearchBar onSearch={handleSearch} />
+<SongList songs={searchResults} onSongSelect={handleSongSelect} />
+{currentSongUrl && <Player currentSongUrl={currentSongUrl} />}
+</div>
+
+);
 }
 
-export default App
+export default App;
